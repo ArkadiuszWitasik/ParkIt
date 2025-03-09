243d9baa029a6ac485282c8db578fed3e1946cc7
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-03-09 21:14:54 +0100

    Create simple layout for auth screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
 
 const SignInScreen = () => {
 	return (
-		<View>
-			<Text>SignInScreen</Text>
-			<Link href={'/sign-up'}>Zarejestruj się</Link>
-			<Link href={'/(tabs)/(home)'}>Zaloguj</Link>
+		<View className="border border-red-700 h-[70%] flex justify-center items-center gap-3">
+			<Text>ParkIt</Text>
+			<TextInput className="bg-gray-300 w-[50%]" placeholder="e-mail" />
+			<TextInput className="bg-gray-300 w-[50%]" placeholder="hasło" />
+			<Link href={'/(tabs)/(home)'} className="bg-gray-300 w-[50%] text-center">
+				Zaloguj się
+			</Link>
+			<Link href={'/sign-up'} className="border border-red-500">
+				Nie masz konta? Utwórz już dziś!
+			</Link>
 		</View>
 	);
 };
--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import React from 'react';
+import { Link } from 'expo-router';
 
 const SignUpScreen = () => {
 	return (
-		<View>
-			<Text>SignUpScreen</Text>
+		<View className="border border-red-700 h-[70%] flex justify-center items-center gap-3">
+			<Text>ParkIt</Text>
+			<TextInput className="bg-gray-300 w-[50%]" placeholder="e-mail" />
+			<TextInput className="bg-gray-300 w-[50%]" placeholder="hasło" />
+			<Link href={'/(tabs)/(home)'} className="bg-gray-300 w-[50%] text-center">
+				Zarejestruj się
+			</Link>
+			<Link href={'/'} className="border border-red-500">
+				Masz już konto? Zaloguj się!
+			</Link>
 		</View>
 	);
 };
