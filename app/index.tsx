d27ed2f595acccfb91ee596a05111a9e80c83d37
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-03-11 21:14:50 +0100

    Add new fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,48 @@
 import { View, Text, TextInput } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 const SignInScreen = () => {
+	const [loaded, error] = useFonts({
+		'BebasNeue-Regular': require('../assets/fonts/BebasNeue-Regular.ttf'),
+		'Raleway-Regular': require('../assets/fonts/Raleway-Regular.ttf'),
+	});
+
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
+		return null;
+	}
+
 	return (
 		<View className="border border-red-700 h-[70%] flex justify-center items-center gap-3">
-			<Text>ParkIt</Text>
+			<Text className="font-BebasNeue text-[40px]">ParkIt</Text>
 			<TextInput
-				className="bg-gray-300 w-[50%] h-[50px]"
+				className="bg-gray-300 w-[50%] h-[50px] font-RalewayRegular"
 				placeholder="e-mail"
 			/>
-			<TextInput className="bg-gray-300 w-[50%] h-[50px]" placeholder="hasło" />
+			<TextInput
+				className="bg-gray-300 w-[50%] h-[50px] font-RalewayRegular"
+				placeholder="hasło"
+			/>
 			<Link
 				href={'/(tabs)/(home)'}
-				className="bg-gray-300 w-[50%] text-center h-[50px]"
+				className="bg-gray-300 w-[50%] text-center h-[50px] font-RalewayRegular"
 			>
 				Zaloguj się
 			</Link>
-			<Link href={'/sign-up'} className="bg-gray-300 w-[50%] h-[50px]">
+			<Link
+				href={'/sign-up'}
+				className="bg-gray-300 w-[50%] h-[50px] font-RalewayRegular"
+			>
 				Nie masz konta? Utwórz już dziś!
 			</Link>
 		</View>
