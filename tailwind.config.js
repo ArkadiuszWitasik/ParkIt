bad5a44b5d4a5d1455ae06b7911853a6155230cc
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-03-13 20:10:25 +0100

    Add SemiBold Raleway font and add color variables to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ module.exports = {
 			fontFamily: {
 				BebasNeueRegular: ['BebasNeue-Regular', 'sans-serif'],
 				RalewayRegular: ['Raleway-Regular', 'sans-serif'],
+				RalewaySemiBold: ['Raleway-SemiBold', 'sens-serif'],
+			},
+			colors: {
+				AppBackground: '#f3f3f3',
+				FontColor: '#212121',
+				Khaki: '#f7d787',
+				Burlywood: '#f7b587',
+				Lightskyblue: '#87c9f7',
+				Thistle: '#ffc7f3',
+				SecoundLayer: '#ffffff',
+				ThirdLayer: '#f3f3f3',
+				GrayFontColor: '#c9cbc9',
 			},
 		},
 	},
