aaef9a08ba53ce7a2c3a321c3826c296db1eee51
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-05-15 22:07:14 +0200

    Change styling and elements distribution in Profile Screen

--- app/(tabs)/(profile)/index.tsx
+++ app/(tabs)/(profile)/index.tsx
@@ -1,12 +1,69 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import UnstyledButton from '@/components/Buttons/UnstyledButton';
+import { Href, router } from 'expo-router';
+import { useUserStore } from '@/store/userStore';
 
 const ProfileScreen = () => {
+	const navigate = (path: Href) => {
+		router.navigate(path);
+	};
+
+	const { user } = useUserStore();
+
+	const tmp = () => {
+		console.log('tmp profile screen');
+	};
+
 	return (
 		<View className="flex-1 m-3 flex flex-col gap-2 items-center">
-			<Text>Portfel</Text>
-			<Text>Samochody</Text>
-			<Text>Premium</Text>
+			<Text className="text-FontColor font-MontserratRegular text-[16px]">
+				Status konta: Premium / Normal
+			</Text>
+			<UnstyledButton
+				onPressFn={() => navigate('/(tabs)/(profile)/loyality')}
+				w="w-full"
+				bgColor="bg-white"
+				bgPressedColor="bg-gray-300"
+				otherStyles="flex justify-center items-center rounded-md"
+			>
+				<Text className="text-FontColor font-MontserratRegular">Premium</Text>
+			</UnstyledButton>
+			<UnstyledButton
+				onPressFn={() => navigate('/(tabs)/(profile)/balance')}
+				w="w-full"
+				bgColor="bg-white"
+				bgPressedColor="bg-gray-300"
+				otherStyles="flex justify-center items-center rounded-md"
+			>
+				<Text className="text-FontColor font-MontserratRegular">Płatności</Text>
+			</UnstyledButton>
+			<UnstyledButton
+				onPressFn={() => navigate('/(tabs)/(profile)/cars')}
+				w="w-full"
+				bgColor="bg-white"
+				bgPressedColor="bg-gray-300"
+				otherStyles="flex justify-center items-center rounded-md"
+			>
+				<Text className="text-FontColor font-MontserratRegular">Samochody</Text>
+			</UnstyledButton>
+			<View className="rounded-md flex flex-col gap-2 bg-white w-full p-3">
+				<Text className="text-FontColor font-RalewaySemiBold text-[18px]">
+					Statystyki
+				</Text>
+				<Text className="text-FontColor font-MontserratRegular text-[16px]">
+					Rezerwacje - {user?.reservations.length}
+				</Text>
+				<Text className="text-FontColor font-MontserratRegular text-[16px]">
+					Zastosowane zniżki
+				</Text>
+				<Text className="text-FontColor font-MontserratRegular text-[16px]">
+					Statystyki aut
+				</Text>
+				<Text className="text-FontColor font-MontserratRegular text-[16px]">
+					Rezerwacje w przeciągu roku
+				</Text>
+			</View>
 		</View>
 	);
 };
