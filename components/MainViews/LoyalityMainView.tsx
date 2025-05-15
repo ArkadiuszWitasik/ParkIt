cc47b4143f53ee8ff22445b21d0a5816d4ff4315
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-05-15 21:27:05 +0200

    Replace mockup data from main screen card to display acctual user data

--- components/MainViews/LoyalityMainView.tsx
+++ components/MainViews/LoyalityMainView.tsx
@@ -1,8 +1,11 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import FireIcon from '@/assets/Icons/FireIcon';
+import { useUserStore } from '@/store/userStore';
 
 const LoyalityMainView = () => {
+	const { user } = useUserStore();
+
 	return (
 		<View className="flex-1 p-3 justify-between ">
 			<View className="flex flex-row justify-between">
@@ -14,50 +17,29 @@ const LoyalityMainView = () => {
 			</View>
 			{/* Loop with amount reservations in color */}
 			<View className="flex flex-row justify-center items-center gap-3">
-				<View className="bg-AppBackground w-[42] h-[42] rounded-[50] flex justify-center items-center">
-					<FireIcon
-						style={{
-							width: 20,
-							height: 20,
-						}}
-					/>
-				</View>
-				<View className="bg-AppBackground w-[42] h-[42] rounded-[50] flex justify-center items-center">
-					<FireIcon
-						style={{
-							width: 20,
-							height: 20,
-						}}
-					/>
-				</View>
-				<View className="bg-AppBackground w-[42] h-[42] rounded-[50] flex justify-center items-center">
-					<FireIcon
-						style={{
-							width: 20,
-							height: 20,
-						}}
-					/>
-				</View>
-				<View className="bg-AppBackground w-[42] h-[42] rounded-[50] flex justify-center items-center">
-					<FireIcon
-						style={{
-							width: 20,
-							height: 20,
-						}}
-					/>
-				</View>
-				<View className="bg-AppBackground w-[42] h-[42] rounded-[50] flex justify-center items-center">
-					<FireIcon
-						style={{
-							width: 20,
-							height: 20,
-						}}
-					/>
-				</View>
+				{[...Array(5)].map((_, index) => (
+					<View
+						key={index}
+						className={`w-[42] h-[42] rounded-[50] flex justify-center items-center ${
+							index < user!.loyalityCount
+								? 'bg-AppPrimaryColor'
+								: 'bg-AppBackground'
+						}`}
+					>
+						<FireIcon
+							style={{
+								width: 20,
+								height: 20,
+							}}
+						/>
+					</View>
+				))}
 			</View>
 			<View className="flex justify-center flex-row items-center">
 				<Text className="font-MontserratRegular text-FontColor">Jeszcze </Text>
-				<Text className="font-MontserratSemiBold text-FontColor">3</Text>
+				<Text className="font-MontserratSemiBold text-FontColor">
+					{5 - user!.loyalityCount}
+				</Text>
 				<Text className="font-MontserratRegular text-FontColor">
 					{' '}
 					rezerwacje by uzyskać{' '}
--- components/MainViews/ReservationsMainView.tsx
+++ components/MainViews/ReservationsMainView.tsx
@@ -2,8 +2,54 @@ import { Text, View } from 'react-native';
 import React from 'react';
 import CalendarIcon from '@/assets/Icons/CalendarIcon';
 import ArrowUpRightIcon from '@/assets/Icons/ArrowUpRightIcon';
+import { useUserStore } from '@/store/userStore';
+import { Timestamp } from 'firebase/firestore';
+import { formatFirebaseTimestamp } from '@/helpers/functions';
+import { useParkingLotsStore } from '@/store/parkingLotsStore';
 
 const ReservationsMainView = () => {
+	const { user } = useUserStore();
+	const { parkingLots } = useParkingLotsStore();
+
+	let closestReservation;
+	if (user) {
+		const today = new Date();
+
+		const activeReservations = user.reservations.filter(
+			(reservation) => reservation.reservationStatus <= 1
+		);
+
+		closestReservation = activeReservations?.reduce((closest, current) => {
+			let tmpClosest: any = closest.reservationDate;
+			let tmpCurrent: any = current.reservationDate;
+
+			let currentDate = new Date(tmpCurrent.toDate());
+			let closestdate = new Date(tmpClosest.toDate());
+
+			const closestDiff = Math.abs(closestdate.getTime() - today.getTime());
+			const currentDiff = Math.abs(currentDate.getTime() - today.getTime());
+
+			return currentDiff < closestDiff ? current : closest;
+		});
+	}
+
+	const { formatedDate, formatedStartTime, formatedEndTime } =
+		formatFirebaseTimestamp(
+			closestReservation!.reservationDate,
+			closestReservation!.reservationStartTime,
+			closestReservation!.reservationEndTime
+		);
+
+	const choosenParkingLotName = parkingLots.find(
+		(parking) => parking.parkingId === closestReservation!.reservationParkingId
+	)?.parkingName;
+
+	const zoneAndSpotSplit = closestReservation!.reservationSpotId?.split('-');
+
+	const choosenZoneAndSpot = zoneAndSpotSplit
+		? zoneAndSpotSplit![2] + ' - ' + zoneAndSpotSplit![1]
+		: ' ';
+
 	return (
 		<View className="p-3 flex flex-col gap-5">
 			<View className="flex flex-row justify-between items-center">
@@ -13,20 +59,32 @@ const ReservationsMainView = () => {
 			</View>
 			<View className="flex flex-row justify-between items-center">
 				<View className="flex-row gap-2 items-center">
-					<View className="bg-AppBackground w-[50] h-[50] rounded-[50] flex justify-center items-center">
-						<CalendarIcon style={{ width: 24, height: 24 }} />
-					</View>
-					<View className="flex gap-1 flex-col">
-						<Text className="font-MontserratBold text-FontColor text-[16px]">
-							Aura
+					{closestReservation ? (
+						<>
+							<View className="bg-AppBackground w-[50] h-[50] rounded-[50] flex justify-center items-center">
+								<CalendarIcon style={{ width: 24, height: 24 }} />
+							</View>
+
+							<View className="flex gap-1 flex-col">
+								<Text className="font-MontserratBold text-FontColor text-[16px]">
+									{choosenParkingLotName}
+								</Text>
+								<Text className="font-MontserratRegular text-FontColor text-[16px]">
+									{choosenZoneAndSpot}
+								</Text>
+								<Text className="font-MontserratRegular text-FontColor text-[16px]">
+									{formatedDate.toLocaleDateString()}{' '}
+									{formatedStartTime.toLocaleTimeString()} -{' '}
+									{formatedEndTime.toLocaleTimeString()}
+								</Text>
+							</View>
+						</>
+					) : (
+						<Text className="w-[90%] font-MontserratRegular text-FontColor">
+							Na twoim koncie nie zarejestrowaliśmy jeszcze żadnych rezerwacji.
+							Przejdź do zakładki rezerwację by dokonać swojej pierwszej!
 						</Text>
-						<Text className="font-MontserratRegular text-FontColor text-[16px]">
-							Parking 15A
-						</Text>
-						<Text className="font-MontserratRegular text-FontColor text-[16px]">
-							17:00 - 20:30
-						</Text>
-					</View>
+					)}
 				</View>
 				<ArrowUpRightIcon style={{ width: 24, height: 24 }} />
 			</View>
