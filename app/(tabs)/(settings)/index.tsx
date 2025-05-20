57e1433e680410c2b843298361c40cc1a9815b55
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-05-20 23:15:26 +0200

    Add Notification and Theme switches in App settings

--- app/(tabs)/(settings)/index.tsx
+++ app/(tabs)/(settings)/index.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Switch } from 'react-native';
+import React, { useState } from 'react';
 import UnstyledButton from '@/components/Buttons/UnstyledButton';
 import { signOut } from 'firebase/auth';
 import { auth } from '@/db/store';
 import { router } from 'expo-router';
 
 const SettingsScreen = () => {
+	const [isNotificationSwitchEnabled, setIsNotificationSwitchEnable] =
+		useState(false);
+	const toggleNotificationSwitch = () =>
+		setIsNotificationSwitchEnable((previousState) => !previousState);
+
+	const [isThemeSwitchEnabled, setIsThemeSwitchEnable] = useState(false);
+	const toggleThemeSwitch = () =>
+		setIsThemeSwitchEnable((previousState) => !previousState);
+
 	const tmp = () => {
 		console.log('tmp settings screen');
 	};
@@ -26,26 +35,32 @@ const SettingsScreen = () => {
 				<Text className="font-RalewaySemiBold text-FontColor text-[16px]">
 					Ustawienia aplikacji
 				</Text>
-				<UnstyledButton
-					onPressFn={() => tmp()}
-					w="w-full"
-					bgColor=""
-					bgPressedColor="bg-gray-300"
-					otherStyles="flex justify-center items-center rounded-md"
-				>
-					<Text className="text-FontColor font-MontserratRegular">
-						Powiadomienia
-					</Text>
-				</UnstyledButton>
-				<UnstyledButton
-					onPressFn={() => tmp()}
-					w="w-full"
-					bgColor=""
-					bgPressedColor="bg-gray-300"
-					otherStyles="flex justify-center items-center rounded-md"
-				>
-					<Text className="text-FontColor font-MontserratRegular">Motyw</Text>
-				</UnstyledButton>
+				<View className="flex justify-center rounded-md w-full h-[50px]">
+					<View className="flex flex-row justify-between items-center">
+						<Text className="text-FontColor font-MontserratRegular">
+							Powiadomienia
+						</Text>
+						<Switch
+							trackColor={{ false: '#767577', true: '#66affa' }}
+							thumbColor={isNotificationSwitchEnabled ? '#fff' : '#f4f3f4'}
+							ios_backgroundColor="#3e3e3e"
+							onChange={toggleNotificationSwitch}
+							value={isNotificationSwitchEnabled}
+						/>
+					</View>
+				</View>
+				<View className="flex justify-center rounded-md w-full h-[50px]">
+					<View className="flex flex-row justify-between items-center">
+						<Text className="text-FontColor font-MontserratRegular">Motyw</Text>
+						<Switch
+							trackColor={{ false: '#767577', true: '#66affa' }}
+							thumbColor={isThemeSwitchEnabled ? '#fff' : '#f4f3f4'}
+							ios_backgroundColor="#3e3e3e"
+							onChange={toggleThemeSwitch}
+							value={isThemeSwitchEnabled}
+						/>
+					</View>
+				</View>
 			</View>
 			<View className="bg-white rounded-md flex flex-col gap-2 p-3">
 				<Text className="font-RalewaySemiBold text-FontColor text-[16px]">
