0ac409edca31e17a330d2bb6d8f094feb715e00d
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-05-21 21:14:09 +0200

    Add firestore folder

---
+++ db/store.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+	apiKey: 'API_KEY',
+	authDomain: 'AUTH_DOMAIN',
+	projectId: 'PROJECT_ID',
+	storageBucket: 'STORAGE_BUCKET',
+	messagingSenderId: 'MESSAGING_SENDER_ID',
+	appId: 'APP_ID',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = initializeAuth(app, {
+	persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+const db = getFirestore(app);
+
+export { db, auth };
